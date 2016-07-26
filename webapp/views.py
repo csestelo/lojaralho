814a455f00eed9b2c4660e597efe0db1b4b1f758
Cintia Sestelo <sestelos@gmail.com>
2016-07-25 21:24:31 -0300

    Add template index

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,6 @@
 from django.http.response import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Lojaralho, sua lojinha do caralho! =)')
+    return render(request, 'index.html')
