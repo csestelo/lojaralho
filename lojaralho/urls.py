f2f0894266af8b0ae585d1e6b1cf851724ce3e97
Cintia Sestelo <sestelos@gmail.com>
2016-07-25 22:11:26 -0300

    Add login and signup views
    Closes #7

--- lojaralho/urls.py
+++ lojaralho/urls.py
@@ -5,4 +5,6 @@ from webapp import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^signup/$', views.signup, name='signup')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,13 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def login(request):
+    pass
+
+
+def signup(request):
+    pass
