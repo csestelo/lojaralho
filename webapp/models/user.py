e960594d96183e4b7111e441aef14f51b1665936
Cintia Sestelo <sestelos@gmail.com>
2016-07-25 22:59:32 -0300

    Add User model

---
+++ webapp/models/user.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import AbstractUser
+from django.db.models import CharField, TextField
+
+
+class User(AbstractUser):
+    address = TextField(null=False, blank=True, default='')
+    cellphone = CharField(null=False, blank=True, default='', max_length=16)
