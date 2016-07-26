bfd3f8326555dce7505ce4962065a22df860c8b1
Cintia Sestelo <sestelos@gmail.com>
2016-07-25 22:31:32 -0300

    Add tests to url

--- webapp/tests.py
+++ webapp/tests.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
-from django.core.urlresolvers import resolve
 from webapp.views import index
 
 
-class TestUrlResolve(TestCase):
-    def test_root_should_point_to_webapp_index(self):
-        self.assertEqual(resolve('/').func, index)
+class TestIndex(TestCase):
+    def test_render_correct_template(self):
+        response = self.client.get('/')
+        template_names = [template.name for template in response.templates]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.resolver_match.func, index)
+        self.assertEqual(template_names, ['index.html'])
