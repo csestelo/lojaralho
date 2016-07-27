fc7ca5cb94c3b867620af21bb85e1b3ef8eb1635
Cintia Sestelo <sestelos@gmail.com>
2016-07-26 22:51:49 -0300

    Add test to welcome view

---
+++ webapp/tests/views/test_welcome.py
@@ -0,0 +1,12 @@
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from webapp.views import welcome
+
+
+class TestIndex(TestCase):
+    def test_render_correct_template(self):
+        response = self.client.get(reverse('welcome'))
+        template_names = [template.name for template in response.templates]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.resolver_match.func, welcome)
+        self.assertEqual(template_names, ['welcome.html', 'index.html'])
