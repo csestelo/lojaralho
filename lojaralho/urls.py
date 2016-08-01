7334ef0c4058f9f4bbf400f5298372deaebc8ec9
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2016-08-01 00:00:24 -0300

    Reorder urls

--- lojaralho/urls.py
+++ lojaralho/urls.py
@@ -5,8 +5,8 @@ from webapp import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^welcome/$', views.welcome, name='welcome'),
+    url(r'^signup/$', views.signup, name='signup'),
     url(r'^login/$', views.login, name='login'),
     url(r'^logout/', views.logout, name='logout'),
-    url(r'^signup/$', views.signup, name='signup'),
     url(r'^admin/', admin.site.urls),
 ]
